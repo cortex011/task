a9b96137add4e715975d83db9f17c259a6746fd9
Serxan <serxan95.elili@gmail.com>
2023-05-09 15:08:00 +0400

    fullversiontask10

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var masinlar = [{
     buraxilisili: 2015
 }
 ]
-console.log('Size uygun masin '+masinlar[0].marka +' '+ masinlar[0].model + ' '+ masinlar[0].buraxilisili + ' -ci il tarixinde istehsal olunub' );
+
 
 var now = 2023;
 
@@ -25,3 +25,14 @@ function age(year){
 
 var yas = age(2014);
 console.log(yas);
+console.log('Size uygun masin '+masinlar[0].marka +' '+ masinlar[0].model + ' '+ masinlar[0].buraxilisili + ' -ci il tarixinde istehsal olunub.Masinin yasi =' + yas );
+
+var list = ['Size uygun masin BMW X5M 2014-cu il tarixinde istehsal olunub', 'Masinin yasi = 9'];
+
+text = '<ul>';
+for(i=0;i<list.length;i++){
+    text +='<li>' + list[i] + '</li>';
+}
+text+= '</ul>';
+
+document.getElementById('container').innerHTML = text;
