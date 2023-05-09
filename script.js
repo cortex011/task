c0d09fb51d2877408be0c3407ec3ff89ffdd0671
Serxan <serxan95.elili@gmail.com>
2023-05-09 14:03:31 +0400

    uncomp

--- script.js
+++ script.js
@@ -1,51 +1,27 @@
-// 1,2.
-var list = new Array();
+var masinlar = [{
+    marka: 'BMW',
+    model: 'X5M',
+    buraxilisili: 2014
+},
+{
+    marka: 'Mercedes',
+    model: 'CLA',
+    buraxilisili: 2016
 
-for(i=20; i<=340; i+=3){
-    
-     if(i%2==0){
+},
+{
+    marka: 'Audi',
+    model: 'RS6',
+    buraxilisili: 2015
+}
+]
+console.log('Size uygun masin '+masinlar[0].marka +' '+ masinlar[0].model + ' '+ masinlar[0].buraxilisili + ' -ci il tarixinde istehsal olunub' );
 
-       if(i==230 || i==200 || i==320 ){
-        continue;
-           
-       }  
-       list.push(i);
+var now = 2023;
 
-      
+function age(year){
+    return now - year;
 }
 
-}
-console.log(list);  
-//3.
-var dbuser = 'serxan';
-var dbpass = '1234';
-var logged = false;
-
-
-while(logged == false){
-    user = prompt('user');
-    pass = prompt('password');
-
-if(dbuser == user && dbpass == pass){
-    alert('Giris edildi');
-    logged = true;
-} else {
-    alert('melumatlar yanlisdir');
-}
-
-}
-
-//4.
-
-var o1 = {
-    "marka": 'Tayota',
-    "model": 'Supra',
-    "yurus": '120000km',
-    "reng": 'Ag'
-}
-
-console.log(o1.marka);
-console.log(o1.model);
-
-
-
+var yas = age(2014);
+console.log(yas);
